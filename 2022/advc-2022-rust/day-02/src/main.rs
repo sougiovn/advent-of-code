24083dcff804628d663375ccc62286748ee865b6
Giovane Souza <giovane.bds@gmail.com>
2022-12-19 17:52:10 +0000

    2022 - day 02 - DONE

--- 2022/advc-2022-rust/day-02/src/main.rs
+++ 2022/advc-2022-rust/day-02/src/main.rs
@@ -11,81 +11,127 @@ fn main() {
     let mut total_score = 0;
 
     for line in input.lines() {
-      let split = line.split_once(" ").unwrap();
-      
-      println!("Round: {:?}", &split);
+        let split = line.split_once(" ").unwrap();
 
-      let opponent_play = Janken::from(split.0);  
+        let opponent_play = Janken::from(split.0);
 
-      let my_play: Janken = Janken::from(split.1);
+        let my_play: Janken = Janken::from(split.1);
 
-      println!("{:?} x {:?}", &opponent_play, &my_play);
+        if my_play > opponent_play {
+            total_score += WIN;
+        } else if my_play == opponent_play {
+            total_score += DRAW;
+        }
 
-      if my_play > opponent_play{
-        total_score += WIN;
-        println!("WIN: {}", &total_score);
-      } else if my_play == opponent_play {
-        total_score += DRAW;
-        println!("DRAW: {}", &total_score);
-      }
-
-      total_score += i32::from(my_play);
-      println!("Round result: {}", &total_score);
-      println!("-----");
+        total_score += i32::from(my_play);
     }
 
-  println!("My total score would be: {}", total_score);
+    println!("My total score would be: {}", total_score);
 }
 
-#[derive(Eq, PartialEq, PartialOrd, Debug)]
+#[derive(Eq, PartialEq, Debug)]
 enum Janken {
     Rock,
     Paper,
-    Scissor,
+    Scissors,
 }
 
+/// Parsing &str values to Janken enum
 impl From<&str> for Janken {
-  fn from(s: &str) -> Janken {
-    match s {
-        "A" => Janken::Rock,
-        "B" => Janken::Paper,
-        "C" => Janken::Scissor,
-        "X" => Janken::Rock,
-        "Y" => Janken::Paper,
-        "Z" => Janken::Scissor,
-        _ => panic!("invalid play")
+    fn from(s: &str) -> Janken {
+        match s {
+            // column 1
+            "A" => Janken::Rock,
+            "B" => Janken::Paper,
+            "C" => Janken::Scissors,
+            // column 2
+            "X" => Janken::Rock,
+            "Y" => Janken::Paper,
+            "Z" => Janken::Scissors,
+            _ => panic!("invalid play"),
+        }
     }
-  }
 }
 
+/// Parsing Janken enum to each of its points
 impl From<Janken> for i32 {
-  fn from(janken: Janken) -> Self {
-      match janken {
-        Janken::Rock => 1,
-        Janken::Paper => 2,
-        Janken::Scissor => 3,
-      }
-  }
+    fn from(janken: Janken) -> Self {
+        match janken {
+            Janken::Rock => 1,
+            Janken::Paper => 2,
+            Janken::Scissors => 3,
+        }
+    }
 }
 
-impl Ord for Janken {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self {
+/// Ordering its win/draw/lose
+impl PartialOrd for Janken {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(match self {
             Self::Rock => match other {
                 Self::Rock => Ordering::Equal,
                 Self::Paper => Ordering::Less,
-                Self::Scissor => Ordering::Greater,
+                Self::Scissors => Ordering::Greater,
             },
             Self::Paper => match other {
                 Self::Rock => Ordering::Greater,
                 Self::Paper => Ordering::Equal,
-                Self::Scissor => Ordering::Less,
+                Self::Scissors => Ordering::Less,
             },
-            Self::Scissor => match other {
+            Self::Scissors => match other {
                 Self::Rock => Ordering::Less,
                 Self::Paper => Ordering::Greater,
-                Self::Scissor => Ordering::Equal,
+                Self::Scissors => Ordering::Equal,
             },
-        }
+        })
+    }
+}
+
+mod test {
+    use crate::Janken;
+
+    // Rock
+    #[test]
+    fn rock_win_scissors() {
+        assert!(Janken::Rock > Janken::Scissors);
+    }
+    #[test]
+    fn rock_draw_rock() {
+        assert!(Janken::Rock == Janken::Rock);
+    }
+
+    #[test]
+    fn rock_lose_paper() {
+        assert!(Janken::Rock < Janken::Paper);
+    }
+
+    // Paper
+    #[test]
+    fn paper_win_rock() {
+        assert!(Janken::Paper > Janken::Rock);
+    }
+    #[test]
+    fn paper_draw_paper() {
+        assert!(Janken::Paper == Janken::Paper);
+    }
+
+    #[test]
+    fn paper_lose_scissors() {
+        assert!(Janken::Paper < Janken::Scissors);
+    }
+
+    // Scissors
+    #[test]
+    fn scissors_win_paper() {
+        assert!(Janken::Scissors > Janken::Paper);
+    }
+    #[test]
+    fn scissors_draw_scissors() {
+        assert!(Janken::Scissors == Janken::Scissors);
+    }
+
+    #[test]
+    fn scissors_lose_rock() {
+        assert!(Janken::Scissors < Janken::Rock);
     }
 }
--- 2022/advc-2022-rust/utils/src/lib.rs
+++ 2022/advc-2022-rust/utils/src/lib.rs
@@ -4,3 +4,8 @@ pub fn get_input() -> String {
   fs::read_to_string("./input")
     .expect("input not found")
 }
+
+pub fn get_sample() -> String {
+  fs::read_to_string("./sample")
+    .expect("sample not found")
+}
